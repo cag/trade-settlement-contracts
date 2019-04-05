aae7b7b546254e188fac667910abc35451edbf0d
Alan Lu <alanlu1023@gmail.com>
2019-04-05 18:38:05 -0500

    fixup: missing mock files

---
+++ test/MockERC1155.sol
@@ -0,0 +1,5 @@
+pragma solidity ^0.5.0;
+
+import { ERC1155Mintable } from "erc-1155/contracts/ERC1155Mintable.sol";
+
+contract MockERC1155 is ERC1155Mintable {}
---
+++ test/MockERC20.sol
@@ -0,0 +1,5 @@
+pragma solidity ^0.5.0;
+
+import { ERC20Mintable } from "openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol";
+
+contract MockERC20 is ERC20Mintable {}
