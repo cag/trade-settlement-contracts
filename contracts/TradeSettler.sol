f94eae8536268d5d6095c90b1e84e004281cb75d
Alan Lu <alanlu1023@gmail.com>
2019-04-05 14:34:56 -0500

    allow depositing ERC20 and ERC1155 tokens

--- contracts/TradeSettler.sol
+++ contracts/TradeSettler.sol
@@ -1,9 +1,37 @@
 pragma solidity ^0.5.0;
 
+import { SafeMath } from "openzeppelin-solidity/contracts/math/SafeMath.sol";
 import { IERC1155 } from "erc-1155/contracts/IERC1155.sol";
+import { IERC1155TokenReceiver } from "erc-1155/contracts/IERC1155TokenReceiver.sol";
 import { IERC20 } from "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
 import { ISignatureValidator } from "@gnosis.pm/safe-contracts/contracts/interfaces/ISignatureValidator.sol";
 
-contract TradeSettler {
-    
+contract TradeSettler is IERC1155TokenReceiver {
+    using SafeMath for uint;
+
+    mapping(address => mapping(address => mapping(uint => uint))) internal balances;
+
+    function balanceOf(address user, address tokenContractAddress, uint tokenId) external view returns (uint) {
+        return balances[user][tokenContractAddress][tokenId];
+    }
+
+    function depositERC20(IERC20 token, uint amount) external {
+        require(token.transferFrom(msg.sender, address(this), amount), "could not transfer erc20 from depositor");
+        balances[msg.sender][address(token)][0] = balances[msg.sender][address(token)][0].add(amount);
+    }
+
+    function onERC1155Received(address _operator, address _from, uint256 _id, uint256 _value, bytes calldata _data) external returns(bytes4) {
+        balances[_operator][msg.sender][_id] = balances[_operator][msg.sender][_id].add(_value);
+        return bytes4(keccak256("onERC1155Received(address,address,uint256,uint256,bytes)"));
+    }
+
+    function onERC1155BatchReceived(address _operator, address _from, uint256[] calldata _ids, uint256[] calldata _values, bytes calldata _data) external returns(bytes4) {
+        require(_ids.length == _values.length, "ids and values arrays length mismatch");
+
+        for(uint i = 0; i < _ids.length; i++) {
+            balances[_operator][msg.sender][_ids[i]] = balances[_operator][msg.sender][_ids[i]].add(_values[i]);
+        }
+
+        return bytes4(keccak256("onERC1155BatchReceived(address,address,uint256[],uint256[],bytes)"));
+    }
 }
--- test/trade-settler.js
+++ test/trade-settler.js
@@ -1,11 +1,118 @@
+const assert = require("assert");
 const TradeSettler = artifacts.require("TradeSettler");
+const ERC20Mintable = artifacts.require("ERC20Mintable");
+const ERC1155Mintable = artifacts.require("ERC1155Mintable");
+
+const { toBN, randomHex } = web3.utils;
 
 contract("TradeSettler", function(accounts) {
-  let tradeSettler;
+  const [
+    operator,
+    erc20Minter,
+    erc1155Minter,
+    erc20Trader,
+    erc1155Trader
+  ] = accounts;
+  const numTokenIds = 10;
+  const tokenIds = [];
+  let tradeSettler, erc20, erc1155;
 
   before(async () => {
-    tradeSettler = await TradeSettler.new();
+    tradeSettler = await TradeSettler.new({ from: operator });
+    erc20 = await ERC20Mintable.new({ from: erc20Minter });
+    erc1155 = await ERC1155Mintable.new();
+    for (let i = 0; i < numTokenIds; i++) {
+      await erc1155.create(0, "", { from: erc1155Minter });
+      tokenIds.push(await erc1155.nonce());
+    }
   });
 
-  it("settles trades");
+  it("allows users to deposit ERC20 tokens", async () => {
+    const amount = toBN(1e18);
+
+    assert.equal(await erc20.balanceOf(erc20Trader), 0);
+    await erc20.mint(erc20Trader, amount, { from: erc20Minter });
+    assert.equal(await erc20.balanceOf(erc20Trader), amount.toString());
+
+    assert.equal(await erc20.balanceOf(tradeSettler.address), 0);
+    await erc20.approve(tradeSettler.address, amount, { from: erc20Trader });
+    await tradeSettler.depositERC20(erc20.address, amount, {
+      from: erc20Trader
+    });
+    assert.equal(await erc20.balanceOf(erc20Trader), 0);
+    assert.equal(
+      await erc20.balanceOf(tradeSettler.address),
+      amount.toString()
+    );
+    assert.equal(
+      await tradeSettler.balanceOf(erc20Trader, erc20.address, 0),
+      amount.toString()
+    );
+  });
+
+  it("allows users to deposit ERC1155 tokens", async () => {
+    async function assert1155BalancesEqual(account, ids, amount) {
+      assert.deepEqual(
+        await erc1155
+          .balanceOfBatch(Array(ids.length).fill(account), ids)
+          .then(vals => vals.map(v => v.toString())),
+        Array(ids.length).fill(amount.toString())
+      );
+    }
+
+    const amount = toBN(1e18);
+
+    await assert1155BalancesEqual(erc1155Trader, tokenIds, 0);
+    await Promise.all(
+      tokenIds.map(id =>
+        erc1155.mint(id, [erc1155Trader], [amount], { from: erc1155Minter })
+      )
+    );
+    await assert1155BalancesEqual(erc1155Trader, tokenIds, amount);
+
+    const [singleTokenId, ...multipleTokenIds] = tokenIds;
+
+    await erc1155.safeTransferFrom(
+      erc1155Trader,
+      tradeSettler.address,
+      singleTokenId,
+      amount,
+      "0x",
+      { from: erc1155Trader }
+    );
+    assert.equal(await erc1155.balanceOf(erc1155Trader, singleTokenId), 0);
+    assert.equal(
+      await erc1155.balanceOf(tradeSettler.address, singleTokenId),
+      amount.toString()
+    );
+    assert.equal(
+      await tradeSettler.balanceOf(
+        erc1155Trader,
+        erc1155.address,
+        singleTokenId
+      ),
+      amount.toString()
+    );
+
+    await erc1155.safeBatchTransferFrom(
+      erc1155Trader,
+      tradeSettler.address,
+      multipleTokenIds,
+      multipleTokenIds.map(() => amount),
+      "0x",
+      { from: erc1155Trader }
+    );
+    await assert1155BalancesEqual(erc1155Trader, multipleTokenIds, 0);
+    await assert1155BalancesEqual(
+      tradeSettler.address,
+      multipleTokenIds,
+      amount
+    );
+    for (const id of multipleTokenIds) {
+      assert.equal(
+        await tradeSettler.balanceOf(erc1155Trader, erc1155.address, id),
+        amount.toString()
+      );
+    }
+  });
 });
