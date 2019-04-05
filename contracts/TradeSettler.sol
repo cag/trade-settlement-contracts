d5917c63bfe113b2e0e494f062ce6053fe7dff23
Alan Lu <alanlu1023@gmail.com>
2019-04-05 12:19:17 -0500

    add 1271 signature validator interface from safe-contracts

--- contracts/TradeSettler.sol
+++ contracts/TradeSettler.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.5.0;
 
 import { IERC1155 } from "erc-1155/contracts/IERC1155.sol";
 import { IERC20 } from "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
+import { ISignatureValidator } from "@gnosis.pm/safe-contracts/contracts/interfaces/ISignatureValidator.sol";
 
 contract TradeSettler {
     
